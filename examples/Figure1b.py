6eaf475da79c7e9a44669934e30736c99483b5b0
Anthony Lamontagne <athony.lamontagne1@ulaval.cas>
2023-05-10 13:53:35 -0400

    essai 3290303

--- examples/Figure1b.py
+++ examples/Figure1b.py
@@ -23,35 +23,39 @@ if __name__ == "__main__":
     horizontal_eqs = (x_expression_horizontal, y_expression_horizontal)
 
     wires = [
-        Wire((26, 26), (26, 42), vertical_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
-        Wire((26, 58), (26, 74), vertical_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
-        Wire((42, 26), (42, 42), vertical_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
-        Wire((42, 42), (42, 58), vertical_eqs, cartesian_variables, HIGH_WIRE_RESISTANCE),
-        Wire((42, 58), (42, 74), vertical_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
-        Wire((58, 26), (58, 42), vertical_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
-        Wire((58, 42), (58, 58), vertical_eqs, cartesian_variables, HIGH_WIRE_RESISTANCE),
-        Wire((58, 58), (58, 74), vertical_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
-        Wire((74, 26), (74, 42), vertical_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
-        Wire((74, 58), (74, 74), vertical_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
+        #Wire((26, 26), (26, 42), vertical_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
+        #Wire((26, 58), (26, 74), vertical_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
+        #Wire((42, 26), (42, 42), vertical_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
+        Wire((42, 26), (42, 74), vertical_eqs, cartesian_variables, HIGH_WIRE_RESISTANCE),
+        #Wire((42, 58), (42, 74), vertical_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
+        #Wire((58, 26), (58, 42), vertical_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
+        Wire((58, 26), (58, 74), vertical_eqs, cartesian_variables, HIGH_WIRE_RESISTANCE),
+        #Wire((58, 58), (58, 74), vertical_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
+        #Wire((74, 26), (74, 42), vertical_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
+        #Wire((74, 58), (74, 74), vertical_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
         Wire((26, 74), (42, 74), horizontal_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
         Wire((42, 74), (58, 74), horizontal_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
         Wire((58, 74), (74, 74), horizontal_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
         Wire((26, 26), (42, 26), horizontal_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
         Wire((42, 26), (58, 26), horizontal_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
         Wire((58, 26), (74, 26), horizontal_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
-        VoltageSource((26, 42), (26, 58), vertical_eqs, cartesian_variables, BATTERY_VOLTAGE),
-        VoltageSource((74, 42), (74, 58), vertical_eqs, cartesian_variables, BATTERY_VOLTAGE)
+        VoltageSource((26, 26), (26, 74), vertical_eqs, cartesian_variables, BATTERY_VOLTAGE),
+        VoltageSource((74, 26), (74, 74), vertical_eqs, cartesian_variables, BATTERY_VOLTAGE)
     ]
-    ground_position = (26, 42)
+    ground_position = (26, 26)
     
 
     circuit = Circuit(wires, ground_position)
     world = World(circuit=circuit, coordinate_system=CoordinateSystem.CARTESIAN, shape=WORLD_SHAPE)
+    #world.show_circuit(
+        #{0: (26, 26), 1: (26, 42), 2: (26, 58), 3: (26, 74), 4: (42, 26), 5: (42, 42), 6: (42, 58), 7: (42, 74), 
+         #8: (58, 26), 9: (58, 42), 10: (58, 58), 11: (58, 74), 12: (74, 26), 13: (74, 42), 14: (74, 58), 15: (74, 74)}
+    #)
     world.show_circuit(
-        {0: (26, 26), 1: (26, 42), 2: (26, 58), 3: (26, 74), 4: (42, 26), 5: (42, 42), 6: (42, 58), 7: (42, 74),
-         8: (58, 26), 9: (58, 42), 10: (58, 58), 11: (58, 74), 12: (74, 26), 13: (74, 42), 14: (74, 58), 15: (74, 74)}
+        {0: (26, 26), 1: (26, 74), 2: (74, 26), 3: (74, 74), 4: (42, 26), 5: (42, 74), 6: (58, 26), 7: (58, 74)}
     )
-    print(world._circuit_voltage[26, 26])
+    
+
     
     world.compute()
     world.show_all()
