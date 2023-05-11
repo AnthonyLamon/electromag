11a26136e18eba8d826ca592e98145fc46b14813
Anthony Lamontagne <athony.lamontagne1@ulaval.cas>
2023-05-10 20:49:39 -0400

    jfk_peaked_me

--- examples/figure2c.py
+++ examples/figure2c.py
@@ -2,24 +2,24 @@ import env_examples  # Modifies path, DO NOT REMOVE
 
 from sympy import Symbol, sqrt
 
-from src import Circuit, CoordinateSystem, VoltageSource, Wire, World
+from src import Circuit, CoordinateSystem, VoltageSource, Wire, World, circuit_node
 
 
 if __name__ == "__main__":
     WORLD_SHAPE = (101, 101)
     BATTERY_VOLTAGE = 1.0
-    HIGH_WIRE_RESISTANCE = 1.0
-    LOW_WIRE_RESISTANCE = 0.01
+    HIGH_WIRE_RESISTANCE = 1
+    LOW_WIRE_RESISTANCE = 0.1
 
     cartesian_variables = Symbol("x"), Symbol("y")
     x, y = cartesian_variables
 
     x_expression_cercle = x
-    y_expression_cercle = (24 - sqrt(576-(x)**2)) 
+    y_expression_cercle = x**2/24
     cercle_inf = (x_expression_cercle, y_expression_cercle)
 
     x_expression_cercle = x
-    y_expression_cercle =  (sqrt(576-(x)**2)-24)
+    y_expression_cercle =  -x**2/24
     cercle_sup = (x_expression_cercle, y_expression_cercle)
     
     x_expression_horizontal = x 
@@ -31,16 +31,24 @@ if __name__ == "__main__":
         Wire((38, 74), (62, 50), cercle_sup, cartesian_variables, LOW_WIRE_RESISTANCE),
         Wire((30, 26), (6, 50), cercle_inf, cartesian_variables, LOW_WIRE_RESISTANCE),
         Wire((30, 74), (6, 50), cercle_sup, cartesian_variables, LOW_WIRE_RESISTANCE),
-        Wire((38, 74), (30, 74), horizontal_eqs, cartesian_variables, HIGH_WIRE_RESISTANCE),
-        VoltageSource((30, 26), (38, 26), horizontal_eqs, cartesian_variables, BATTERY_VOLTAGE)
+        VoltageSource((30, 26), (38, 26), horizontal_eqs, cartesian_variables, BATTERY_VOLTAGE),
+        Wire((38, 74), (30, 74), horizontal_eqs, cartesian_variables, HIGH_WIRE_RESISTANCE)
     ]
     ground_position = (30, 26)
 
     circuit = Circuit(wires, ground_position)
-    world = World(circuit=circuit, coordinate_system=CoordinateSystem.CARTESIAN, shape=WORLD_SHAPE)
-    world.show_circuit(
-        {0: (38, 26), 1: (62, 50), 2: (30, 26), 3: (6, 50), 4: (30, 74), 5: (38, 74) }
-    )
+    #world = World(circuit=circuit, coordinate_system=CoordinateSystem.CARTESIAN, shape=WORLD_SHAPE)
+    #world.show_circuit(
+      #  {0: (38, 26), 1: (62, 50), 2: (30, 26), 3: (6, 50), 4: (30, 74), 5: (38, 74) }
+    #)
     
-    world.compute()
-    world.show_all()
+
+
+    #world.compute()
+    #world.show_all()
+    print(circuit.has_voltage_sources)
+    print(circuit.is_closed)
+    print(circuit.nodes)
+    #print(circuit._set_potentials())
+
+    #l'erreur à l'air d etre ici "component.current*component.resistance" ligne 268 de circuit. Il n'y a pas l'air d'avoir de courant (NoneType)
